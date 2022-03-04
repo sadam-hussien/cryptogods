006387e6a924d776b5df94b48b9580ed83903f98
sadam-hussien <m.hus.dev@gmail.com>
2022-03-04 21:49:41 +0200

    roadmap

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+// Some random colors
+const colors = ["#FADC94"];
+
+const numBalls = 100;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = "0.3em";
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12,
+  };
+
+  // let anim = el.animate(
+  //   [
+  //     { transform: "translate(0, 0)" },
+  //     { transform: `translate(${to.x}rem, ${to.y}rem)` },
+  //   ],
+  //   {
+  //     duration: (Math.random() + 1) * 2000, // random duration
+  //     direction: "alternate",
+  //     fill: "both",
+  //     iterations: Infinity,
+  //     easing: "ease-in-out",
+  //   }
+  // );
+});
+
 $(function () {
   var aboutSwiper = new Swiper(".about-swiper", {
     pagination: {
@@ -75,62 +117,72 @@ $(function () {
     });
   });
 
-  var roadmapSwiper = new Swiper(".roadmap-swiper-boxes", {
-    slidesPerView: 5,
-    spaceBetween: 0,
-    centeredSlides: true,
-    initialSlide: 1,
-    allowTouchMove: false,
-    navigation: {
-      nextEl: ".roadmap-navigation-next",
-      prevEl: ".roadmap-navigation-prev",
-    },
-    on: {
-      init: function () {
-        $(".roadmap .roadmap-first-swiper").addClass("my-active-swiper");
+  function roadMapSwiper(el, destroy) {
+    // ".roadmap-swiper-boxes"
+    var s = new Swiper(el, {
+      slidesPerView: 5,
+      spaceBetween: 0,
+      centeredSlides: true,
+      initialSlide: 1,
+      allowTouchMove: false,
+      navigation: {
+        nextEl: ".roadmap-navigation-next",
+        prevEl: ".roadmap-navigation-prev",
+      },
+      on: {
+        init: function () {
+          $(".roadmap .roadmap-first-swiper").addClass("my-active-swiper");
 
-        let activeIndex = this.activeIndex;
-        if (activeIndex == 1) {
-          $(".roadmap-navigation-prev").addClass(
-            "roadmap-navigation-prev-disabled"
-          );
-        } else {
-          $(".roadmap-navigation-prev").removeClass(
-            "roadmap-navigation-prev-disabled"
-          );
-        }
-      },
-      slideChange: function () {
-        if (this.activeIndex == 1) {
-          $(".roadmap-navigation-prev").addClass(
-            "roadmap-navigation-prev-disabled"
-          );
-        } else {
-          $(".roadmap-navigation-prev").removeClass(
-            "roadmap-navigation-prev-disabled"
-          );
-        }
-        if (this.activeIndex > 1) {
-          $(".roadmap .roadmap-first-swiper").removeClass("my-active-swiper");
           let activeIndex = this.activeIndex;
-          let slides = this.slides;
-          slides.each(function (item, index) {
-            console.log(index, activeIndex);
-            if (activeIndex === slides.length - 1) {
-              $(this).addClass("my-last-swiper");
-            } else {
-              $(this).removeClass("my-last-swiper");
-            }
-            if (activeIndex > index) {
-              $(this).addClass("my-prev-swiper");
-            } else {
-              $(this).removeClass("my-prev-swiper");
-            }
-          });
-        }
+          if (activeIndex == 1) {
+            $(".roadmap-navigation-prev").addClass(
+              "roadmap-navigation-prev-disabled"
+            );
+          } else {
+            $(".roadmap-navigation-prev").removeClass(
+              "roadmap-navigation-prev-disabled"
+            );
+          }
+        },
+        slideChange: function () {
+          if (this.activeIndex == 1) {
+            $(".roadmap-navigation-prev").addClass(
+              "roadmap-navigation-prev-disabled"
+            );
+          } else {
+            $(".roadmap-navigation-prev").removeClass(
+              "roadmap-navigation-prev-disabled"
+            );
+          }
+          if (this.activeIndex > 1) {
+            $(".roadmap .roadmap-first-swiper").removeClass("my-active-swiper");
+            let activeIndex = this.activeIndex;
+            let slides = this.slides;
+            slides.each(function (item, index) {
+              console.log(index, activeIndex);
+              if (activeIndex === slides.length - 1) {
+                $(this).addClass("my-last-swiper");
+              } else {
+                $(this).removeClass("my-last-swiper");
+              }
+              if (activeIndex > index) {
+                $(this).addClass("my-prev-swiper");
+              } else {
+                $(this).removeClass("my-prev-swiper");
+              }
+            });
+          }
+        },
       },
-    },
-  });
+    });
+    if (destroy) {
+      s.destroy();
+    }
+  }
+  roadMapSwiper(".roadmap-swiper-boxes-1");
+  // roadMapSwiper(".roadmap-swiper-boxes-2");
+  // roadMapSwiper(".roadmap-swiper-boxes-3");
+  // roadMapSwiper(".roadmap-swiper-boxes-4");
 
   // nav
   $(".main-header .nav-item").on("click", function () {
@@ -142,11 +194,19 @@ $(function () {
     });
   });
 
+  // $(".roadmap-tab .roadmap-tab-1").click();
+
   // tabs
   $(".roadmap-tab").on("click", function () {
+    let id = $(this).attr("data-tab");
+    // tab-3
+    let idSplit = parseInt(id.slice(4)) - 1; // 2
+    console.log(idSplit);
+    // tab-
+    roadMapSwiper(`#${"tab-" + idSplit} .roadmap-swiper-boxes`, true);
+    roadMapSwiper(`#${id} .roadmap-swiper-boxes`);
     $(".roadmap-tab").removeClass("active");
     $(this).addClass("active");
-    let id = $(this).attr("data-tab");
     const activeTab = $(`#${id}`);
     $(".roadmap-box-outer").removeClass("active");
     activeTab.addClass("active");
